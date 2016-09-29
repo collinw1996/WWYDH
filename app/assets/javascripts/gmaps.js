bfc05592dc3b241b0faafcf34f03d0ba34ea734f
manishjoshi12 <mjoshi1@students.towson.edu>
2016-09-29 13:51:03 -0400

    Update gmaps.js

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -105,7 +105,7 @@ function loadScript() {
 	console.log("map loading ...");
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  //'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBJYFdplGeKUUEmGZ-vL4ydiSZ09Khsa_o&sensor=false&libraries=drawing'
+  //'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyANe409_h7ySY9h80bWYjeZyeKcKwDwX78&sensor=false&libraries=drawing'
   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
     //'&v=3.14'+
     //'&key=AIzaSyBJYFdplGeKUUEmGZ-vL4ydiSZ09Khsa_o'+
