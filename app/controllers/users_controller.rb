761e0a1c05e05145b2180aaa492e82c5223a1344
collinw1996 <cwoodr4@students.towson.edu>
2016-10-20 12:01:31 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,14 +49,11 @@ class UsersController < ApplicationController
 
   def confirm_email
     user = User.find_by_confirm_token(params[:id])
-    if user
-      user.email_activate
-      flash[:success] = "Welcome to WWYDH! Your accout has been confirmed."
+   
+     
+     flash[:success] = "Welcome to WWYDH! Your accout has been confirmed."
       redirect_to root_url
-    else
-      flash[:error] = "User does not exist."
-      redirect_to root_url
-    end
+    
   end
 
 	private
